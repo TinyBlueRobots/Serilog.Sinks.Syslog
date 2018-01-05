406cac464ca287d3f1966fe0e985a07c43dc0f47
Jon Canning <jon.canning@gmail.com>
2018-01-05 07:19:40 +0000

    Support net45

--- pack.sh
+++ pack.sh
@@ -1,4 +1,4 @@
 export FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.5/
-NUGETVERSION=1.0.7
+NUGETVERSION=1.0.8
 dotnet pack Serilog.Sinks.Syslog -c Release /p:PackageVersion=$NUGETVERSION
 dotnet nuget push Serilog.Sinks.Syslog/bin/Release/Serilog.Sinks.SyslogServer.$NUGETVERSION.nupkg -k $NUGETKEY -s nuget.org
